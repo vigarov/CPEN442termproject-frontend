fbe131ef65f69ed7ad5403928bf826b7bee9fec2
Victor G <vesbru@yahoo.fr>
2022-11-02 18:13:16 -0700

    working project structure

--- backend.py
+++ backend.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='./frontend/templates',static_folder='./frontend/static')
 
 
 @app.route('/')
